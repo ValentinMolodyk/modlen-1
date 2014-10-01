139c4064f00b1a2c14946548b2d7230a23d73328
Roman Iakobchuk <kabom.ua@gmail.com>
2014-10-01 22:05:22 +0300

    hope to deploy 2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@178.62.234.82}
-role :web, %w{deploy@178.62.234.82}
-role :db,  %w{deploy@178.62.234.82}
+role :app, %w{root@178.62.132.231}
+role :web, %w{root@178.62.132.231}
+role :db,  %w{root@178.62.132.231}
 
 
 # Extended Server Syntax
