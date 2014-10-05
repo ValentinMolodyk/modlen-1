efe27f0314564c68a7ad8d15ebc1f6a9c118a9c2
Roman Iakobchuk <kabom.ua@gmail.com>
2014-10-05 18:06:13 +0300

    repair assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  #config.assets.compile = true
+  #config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 end
