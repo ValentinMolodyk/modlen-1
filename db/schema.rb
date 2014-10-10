f1211e11f430080b8dd2adfa168d8601d58f3bbd
Roman <roman@Romans-MacBook-Air.local>
2014-10-10 11:09:02 +0300

    berfore merge with Valik

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20141005190148) do
   create_table "currencies", force: true do |t|
     t.string   "name"
     t.string   "code"
-    t.decimal  "rate",       precision: 10, scale: 10
+    t.decimal  "rate",       precision: 4, scale: 3
     t.datetime "created_at"
     t.datetime "updated_at"
   end
